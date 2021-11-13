a431ff1005e3d5377f6c26e76992e503423c932d
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-13 23:51:55 +0000

    Move setupInput function to new input file

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    console.log("Connection terminated!");
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const connect = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect();
+setupInput();
